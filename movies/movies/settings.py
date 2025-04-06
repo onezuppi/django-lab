be8bcf4247baaf494477c34dc65ed69c94305337
Sergey Dorokhov <seregapax@gmail.com>
2025-04-06 16:02:48 +0500

    lab 5

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'moviesapp',
-    'products'
+    'products',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')    ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- movies/movies/urls.py
+++ movies/movies/urls.py
@@ -22,9 +22,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include("moviesapp.urls")),
-
+    path("blog/", include("blog.urls")),
 ]
 
 if settings.DEBUG:
